0179dc1911d412b26ba3710812c5c34062ce5068
JoBo Gamer <78912075+RealJoBoGamer@users.noreply.github.com>
2023-03-28 11:15:28 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,23 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    basic.showIcon(IconNames.SmallDiamond)
+    if (receivedNumber == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            # # # # #
+            `)
+    } else if (receivedNumber == 0) {
+        basic.showIcon(IconNames.Square)
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Yes)
+    radio.sendNumber(0)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Yes)
+    radio.sendNumber(1)
+})
+radio.setGroup(255)
